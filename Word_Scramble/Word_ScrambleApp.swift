5c9968681de6d221e9dae6a097aeb35c4c4a3c70
Kyle <kylegranthousel@gmail.com>
2024-04-23 10:29:10 -0400

    Initial Commit

---
+++ Word_Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word_Scramble
+//
+//  Created by Kyle Housel on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
