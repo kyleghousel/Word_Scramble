c87d7a886bf283f33a56cfe71e2238ce261dc242
Kyle <kylegranthousel@gmail.com>
2024-05-01 09:12:37 -0400

    Initial commit on this branch

---
+++ Word_Scramble/Word_Scramble_model_logic.swift
@@ -0,0 +1,13 @@
+//
+//  Word_Scramble_model_logic.swift
+//  Word_Scramble
+//
+//  Created by Kyle Housel on 4/30/24.
+//
+
+import Foundation
+import SwiftUI
+
+class WordScrambleGame {
+    // Define properties and methods for game logic
+}
